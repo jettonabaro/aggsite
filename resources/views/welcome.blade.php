12894fa00aa5606d0933602df1ffb9432e22f668
Jett Navarro <Jett Navarro>
2016-09-12 12:08:15 +0800

    Added my project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,226 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">    
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>All by God's Grace Church</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    
+    <link href="bootstrap3/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/gsdk.css" rel="stylesheet" />  
+    <link href="assets/css/demo.css" rel="stylesheet" /> 
+    
+    <!--     Font Awesome     -->
+    <link href="bootstrap3/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div id="navbar-full">
+    <div class="container">
+        <nav class="navbar navbar-ct-green navbar-transparent navbar-fixed-top" role="navigation">
+          
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="#">
+                     <div class="logo-container">
+                        <div class="logo">
+                            <img src="assets/img/logo.jpg">
+                        </div>
+                        <div class="brand">
+                            AGGC Worldwide
+                        </div>
+                    </div>
+                </a>
+            </div>
+        
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ministries <b class="caret"></b></a>
+                          <ul class="dropdown-menu">
+                            <li><a href="#">Multimedia</a></li>
+                            <li><a href="#">Worship</a></li>
+                            <li><a href="#">Youth</a></li>
+                          </ul>
+                    </li>
+                    <li><a href="#">Outreaches</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Chapters <b class="caret"></b></a>
+                          <ul class="dropdown-menu">
+                            <li><a href="#">Bataan (Balanga)</a></li>
+                            <li><a href="#">Bataan (Hermosa)</a></li>
+                            <li><a href="#">Bulacan</a></li>
+                            <li><a href="#">Cavite</a></li>
+                            <li><a href="#">Davao</a></li>
+                            <li><a href="#">Dipolog</a></li>
+                            <li><a href="#">Manila</a></li>
+                            <li><a href="#">Pampanga</a></li>
+                            <li><a href="#">Zamboanga</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Australia</a></li>
+                            <li><a href="#">United States of America</a></li>
+                            <li><a href="#">Qatar</a></li>
+                          </ul>
+                    </li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Archives</a></li>
+               </ul>
+              
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+    </div><!--  end container-->
+    
+    <div class='blurred-container'>
+        <div class="motto">
+            <div>2002</div>
+            <div class="border">AGGC</div>
+            <div>2016</div>
+        </div>
+        <div class="img-src" style="background-image: url('assets/img/cover2.jpg')"></div>
+        <div class='img-src blur' style="background-image: url('assets/img/cover2.jpg')"></div>
+    </div>
+    
+</div>    
+
+<div class="main">
+    <div class="container tim-container">
+        <div id="extras">
+            <div class="space"></div>
+            <div class="row">
+                <div class="col-md-7 col-md-offset-0 col-sm-10 col-sm-offset-1">
+                    <div class="text-center">
+                        <img src="assets/img/pas-melvin.jpg" alt="Rounded Image" class="img-rounded img-responsive img-dog">
+                    </div>
+                </div>
+                <div class="col-md-5 col-sm-12">
+                                <h1 class="text-center">Pastor Melvin Samonte
+                                
+                                <small class="subtitle">Grace</small></h1>
+                                <hr>
+                                <p>
+                                <b>Lorem ipsum</b> dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.                                 
+                                </p>
+
+                </div>
+            </div>
+        </div>
+    <!--     end extras -->    
+    </div>
+<!-- end container -->
+<div class="space-30"></div>
+</div> 
+    
+
+
+
+<div id="carousel">
+    <!--    
+            IMPORTANT - This carousel can have a special class for a smooth transition "gsdk-transition". Since javascript cannot be overwritten, if you want to use it, you can use the bootstrap.js or bootstrap.min.js from the GSDKit or you can open your bootstrap.js file, search for "emulateTransitionEnd(600)" and change it with "emulateTransitionEnd(1200)"     
+            
+    -->
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      </ol>
+    
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="assets/img/car/1.png" alt="Awesome Image">
+        </div>
+        <div class="item">
+          <img src="assets/img/car/2.png" alt="Awesome Image">
+        </div>
+        <div class="item">
+          <img src="assets/img/car/3.png" alt="Awesome Image">
+        </div> 
+        <div class="item">
+          <img src="assets/img/car/4.png" alt="Awesome Image">
+        </div>
+        <div class="item">
+          <img src="assets/img/car/5.png" alt="Awesome Image">
+        </div>  
+      </div>
+    
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="fa fa-angle-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="fa fa-angle-right"></span>
+      </a>
+    </div>
+</div> <!-- end carousel -->
+
+<div class="space-30"></div>
+</div>
+
+
+<!-- end main -->
+
+
+
+</body>
+
+    <script src="jquery/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+
+    <script src="bootstrap3/js/bootstrap.js" type="text/javascript"></script>
+    <script src="assets/js/gsdk-checkbox.js"></script>
+    <script src="assets/js/gsdk-radio.js"></script>
+    <script src="assets/js/gsdk-bootstrapswitch.js"></script>
+    <script src="assets/js/get-shit-done.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+<script type="text/javascript">
+         
+    $('.btn-tooltip').tooltip();
+    $('.label-tooltip').tooltip();
+    $('.pick-class-label').click(function(){
+        var new_class = $(this).attr('new-class');  
+        var old_class = $('#display-buttons').attr('data-class');
+        var display_div = $('#display-buttons');
+        if(display_div.length) {
+        var display_buttons = display_div.find('.btn');
+        display_buttons.removeClass(old_class);
+        display_buttons.addClass(new_class);
+        display_div.attr('data-class', new_class);
+        }
+    });
+    $( "#slider-range" ).slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [ 75, 300 ],
+    });
+    $( "#slider-default" ).slider({
+            value: 70,
+            orientation: "horizontal",
+            range: "min",
+            animate: true
+    });
+    $('.carousel').carousel({
+      interval: 4000
+    });
+      
+    
+</script>
+</html>
